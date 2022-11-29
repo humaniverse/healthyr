1eb79d38daa5bcfa08bcaa4512ea13335d4e8ec3
Victoria Latham <vickilatham16@gmail.com>
2022-11-29 16:04:35 +0000

    edited comments to make clearer

--- data-raw/wales-critical-general-acute-beds.R
+++ data-raw/wales-critical-general-acute-beds.R
@@ -18,12 +18,11 @@ raw <- statswales_get_dataset("hlth0310")
 wales_critical_general_acute_beds <-
   raw |>
   as_tibble() |> 
-  # Filter to only include data from Jan 2021
+  # Filter to only include data from Jan 2021 and only include high level specialty categories
   filter(Month_Code >= "202101" &
-           # Filter to only include high level specialty categories
            Specialty_Hierarchy == "0"
          ) |>
-  # Grouping specialties of less relevance together
+  # Renaming measure variable names
   mutate(
          measure = case_when(Measure_ItemName_ENG == "Average daily occupied beds" ~ "average_daily_beds_occupied",
                              Measure_ItemName_ENG == "Average daily available beds" ~ "average_daily_beds_available",

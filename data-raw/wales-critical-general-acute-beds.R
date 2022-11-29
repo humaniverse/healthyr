374e7c1156952902e05f33d33273ecc8e2b7ef95
Victoria Latham <vickilatham16@gmail.com>
2022-11-29 14:06:11 +0000

    edits to beds code

--- data-raw/wales-critical-general-acute-beds.R
+++ data-raw/wales-critical-general-acute-beds.R
@@ -9,7 +9,7 @@ library(tidyr)
 library(lubridate)
 
 
-# ---- Function to download data ----
+# ---- Function to download data from Stats Wales website ----
 raw <- statswales_get_dataset("hlth0310")
 
 
@@ -20,42 +20,25 @@ wales_beds <-
   # Filter to only include data from Jan 2021
   filter(Month_Code >= "202101" &
            # Filter to only include high level specialty categories
-           Specialty_Hierarchy == "0") |>
+           Specialty_Hierarchy == "0"
+         ) |>
   # Grouping specialties of less relevance together
-  mutate(Specialty_Name = case_when(Specialty_ItemName_ENG == "Bone Marrow Unit" | Specialty_ItemName_ENG == "Special Care Baby Units" | 
-                                 Specialty_ItemName_ENG == "Community Medicine" | Specialty_ItemName_ENG == "Occupational Medicine" | 
-                                 Specialty_ItemName_ENG == "Learning Disability" | Specialty_ItemName_ENG == "Nursing Activity" | 
-                                 Specialty_ItemName_ENG == "Pathology" ~ "Other Specialties",
-                               TRUE ~ Specialty_ItemName_ENG),
-         # Renaming measure variable
+  mutate(
          measure = case_when(Measure_ItemName_ENG == "Average daily occupied beds" ~ "average_daily_beds_occupied",
                              Measure_ItemName_ENG == "Average daily available beds" ~ "average_daily_beds_available",
-                             Measure_ItemName_ENG == "Percentage occupancy" ~ "beds_occupancy_rate")) |> 
+                             Measure_ItemName_ENG == "Percentage occupancy" ~ "beds_occupancy_rate")
+         ) |> 
   select(
-    Date = Month_ItemName_ENG, 
-    Hospital_code = Organisation_Code,
-    Hospital_name = Organisation_ItemName_ENG,
-    Specialty_Name, 
-    Measure, Data) |>
-  pivot_wider(names_from = c(Specialty_Name, Measure), values_from = Data, values_fn = list)
-
-
-wales_amublance_services <-
-  ambo |>
-  select(
-    Date = Date_ItemName_ENG,
-    HB_code = Area_Code,
-    HB = Area_ItemName_ENG,
-    Measure_ItemName_ENG,
-    Data
-  ) |>
-  pivot_wider(names_from = Measure_ItemName_ENG, values_from = Data) |>
-  filter(HB != "WALES") |>
-  select(-Date)
-
+    date = Month_ItemName_ENG, 
+    hospital_code = Organisation_Code,
+    hospital_name = Organisation_ItemName_ENG,
+    specialty_name, 
+    measure, Data
+    ) |>
+  pivot_wider(names_from = c(specialty_name, measure), values_from = Data, values_fn = list)
 
 wales_beds |>
-  write_rds("preprocess/data/wales_beds.rds")
+  write_rds("data/wales_beds.rds")
 
 # Save output to data/ folder
 usethis::use_data(wales_beds, overwrite = TRUE)

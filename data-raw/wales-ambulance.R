bc4b4524b93ccb184cecfd5aeedfdf67d23b644b
Victoria Latham <vickilatham16@gmail.com>
2022-11-29 15:09:38 +0000

    added script to pull and clean welsh ambulance data

---
+++ data-raw/wales-ambulance.R
@@ -0,0 +1,33 @@
+# ---- Load ----
+library(statswalesr)
+library(tidyverse)
+library(jsonlite)
+library(httr)
+library(RCurl)
+library(tidyr)
+library(lubridate)
+
+
+# ---- Function to download data from Stats Wales website ----
+raw <- raw <- statswales_get_dataset("hlth1308")
+
+# ---- Clean data ----
+wales_amublance_services <-
+  raw |>
+  as_tibble() |>
+  # Filter to only include data from Jan 2021
+  filter(Date_SortOrder >= "202101") |> 
+  select(
+    Date = Date_ItemName_ENG,
+    HB_code = Area_Code,
+    HB = Area_ItemName_ENG,
+    Measure_ItemName_ENG,
+    Data
+  ) |>
+  pivot_wider(names_from = Measure_ItemName_ENG, values_from = Data)
+
+wales_amublance_services |>
+  write_rds("data/wales_ambulance_services.rds")
+
+# Save output to data/ folder
+usethis::use_data(wales_amublance_services, overwrite = TRUE)

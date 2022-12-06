fc5a554ea3945d4aa28b6d7f30d3a38bfdf78a4b
VictoriaLatham <victorialatham@redcross.org.uk>
2022-12-06 13:54:01 +0000

    added pivot_longer/wider to bed file

--- data-raw/wales-ambulance-waiting-times.R
+++ data-raw/wales-ambulance-waiting-times.R
@@ -6,17 +6,17 @@ library(httr)
 library(RCurl)
 library(tidyr)
 library(lubridate)
-
+library(janitor)
 
 # ---- Function to download data from Stats Wales website ----
-raw <- raw <- statswales_get_dataset("hlth1308")
+raw <- statswales_get_dataset("hlth1308")
 
 # ---- Clean data ----
 wales_ambulance_waiting_times <-
   raw |>
   as_tibble() |>
   # Filter to only include data from Jan 2021
-  filter(Date_SortOrder >= "202101") |> 
+  filter(Date_SortOrder >= "202101") |>
   select(
     Date = Date_ItemName_ENG,
     HB_code = Area_Code,
@@ -24,9 +24,8 @@ wales_ambulance_waiting_times <-
     Measure_ItemName_ENG,
     Data
   ) |>
-  pivot_wider(names_from = Measure_ItemName_ENG, values_from = Data) |> 
+  pivot_wider(names_from = Measure_ItemName_ENG, values_from = Data) |>
   clean_names()
-
 
 # Save output to data/ folder
 usethis::use_data(wales_ambulance_waiting_times, overwrite = TRUE)

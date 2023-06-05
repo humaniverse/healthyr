e4a6c47307ffadae55a2c1a7bbbb815aad8715af
Jenna Tan <jennajltan@gmail.com>
2023-06-05 14:02:05 +0100

    .DS_Store file removed; code restyled

--- data-raw/england-aande.R
+++ data-raw/england-aande.R
@@ -10,13 +10,12 @@ load_all(".")
 
 # ---- Function to download and clean ----
 scrape_data <- function(id, sheet, range, date, col_index) {
-
   # get url from query_urls table
   query_url <-
     query_urls |>
     filter(id == {{ id }}) |>
     pull(query)
-  
+
   # download xls to as a temp file
   download <- tempfile(fileext = ".xls")
 
@@ -33,15 +32,22 @@ scrape_data <- function(id, sheet, range, date, col_index) {
 
   # clean the df
   data <- raw |>
-    select(all_of(col_index))  |> # select relevant columns
-    rename(code = 1, total_attendances = 2, attendances_over_4hours = 3, total_emergency_admissions = 4, emergency_admissions_over_4hours = 5) |> # rename columns
+    select(all_of(col_index)) |> # select relevant columns
+    rename(
+      code = 1, 
+      total_attendances = 2, 
+      attendances_over_4hours = 3, 
+      total_emergency_admissions = 4, 
+      emergency_admissions_over_4hours = 5
+    ) |> # rename columns
     slice(-1) |> # remove first row
     filter(!is.na(code)) |> # remove NAs
     mutate_at(2:5, as.numeric) |> # change cols datatype to numeric
-    mutate(pct_attendance_over_4hours = round(attendances_over_4hours / total_attendances, 2),
-           pct_emergency_admissions_over_4hours = round(emergency_admissions_over_4hours/ total_emergency_admissions, 2),
-           date = date
-           ) # add percentage and date columns
+    mutate(
+      pct_attendance_over_4hours = round(attendances_over_4hours / total_attendances, 2),
+      pct_emergency_admissions_over_4hours = round(emergency_admissions_over_4hours / total_emergency_admissions, 2),
+      date = date
+    ) # add percentage and date columns
 
   return(data)
 }
@@ -56,7 +62,7 @@ icb_df <-
     sheet = rep("System Level Data", 12),
     range = rep("B16:AA61", 12),
     date = c("April 2022", "May 2022", "June 2022", "July 2022", "August 2022", "September 2022", "October 2022", "November 2022", "December 2022", "January 2023", "February 2023", "March 2023"),
-    col_index = rep(list(c(1,6,14,24,25)), 12)
+    col_index = rep(list(c(1, 6, 14, 24, 25)), 12)
   )
 
 # Build dataframe with all months
@@ -75,12 +81,11 @@ trust_df <-
     sheet = rep("Provider Level Data", 12),
     range = rep("B16:AB223", 12),
     date = c("April 2022", "May 2022", "June 2022", "July 2022", "August 2022", "September 2022", "October 2022", "November 2022", "December 2022", "January 2023", "February 2023", "March 2023"),
-    col_index = rep(list(c(1,7,15,25,26)), 12)
+    col_index = rep(list(c(1, 7, 15, 25, 26)), 12)
   )
 
 # Build dataframe with all months
-england_trust_accidents_emergency <- pmap_dfr(trust_df, scrape_data) 
+england_trust_accidents_emergency <- pmap_dfr(trust_df, scrape_data)
 
 # Save output to data/ folder
 usethis::use_data(england_trust_accidents_emergency, overwrite = TRUE)
-
